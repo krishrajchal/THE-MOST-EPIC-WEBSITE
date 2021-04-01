2ca26c86c8d19c8ba44fc53081a3c6efad194cfb
krishrajchal <67868262+krishrajchal@users.noreply.github.com>
2021-04-01 17:58:04 -0600

    NOT A RICK ROLL
    !!!!!!!!11!!1!1!

---
+++ j.js
@@ -0,0 +1,26 @@
+for (var i=0;i<20;i++){
+	var a = document.createElement("iframe");
+	a.id = i.toString();
+	a.style.display = "none";
+	a.style.width = "100";
+	a.style.height = "50";
+	a.src = "https://www.youtube.com/embed/dQw4w9WgXcQ";
+	document.body.appendChild(a);
+}
+document.getElementById("a").onclick = function(){
+	ssss();
+}
+
+function ssss(){
+	for(var i = 0; i<20; i++){
+		var a = document.getElementById(i.toString());
+		if(a.style.display == "none"){
+			a.style.display = "block";
+			a.src = "https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1";
+		}
+		else{
+			a.style.display = "none";
+			a.src = "https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1";
+		}
+	}
+}
