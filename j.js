cbe072b1b717b994e34e427bddb9f95482639d03
krishrajchal <67868262+krishrajchal@users.noreply.github.com>
2021-04-02 16:40:22 -0600

    Add files via upload

--- j.js
+++ j.js
@@ -1,37 +1,11 @@
-for (var i=0;i<20;i++){
+var site = "https://www.youtube.com/embed/FtE6SV_1wu4";
+
+for (var i=0;i<21;i++){
 	var a = document.createElement("iframe");
 	a.id = i.toString();
 	a.style.display = "none";
 	a.style.width = "100";
 	a.style.height = "50";
-	a.src = "https://www.youtube.com/embed/dQw4w9WgXcQ";
+	a.src = site+"?autoplay=1";
 	document.body.appendChild(a);
-}
-for (var i=0;i<20;i++){
-	document.getElementById(i.toString()).src = "https://www.youtube.com/embed/dQw4w9WgXcQ";
-}
-
-var c = document.createElement("h1");
-c.id = "g";
-c = document.getElementById("g");
-c.style.display="none";
-
-document.getElementById("a").onclick = function(){
-	ssss();
-}
-
-function ssss(){
-	for(var i = 0; i<20; i++){
-		var a = document.getElementById(i.toString());
-		if(a.style.display == "none"){
-			a.style.display = "block";
-			c.style.display = "block";
-			a.src = "https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1";
-		}
-		else{
-			a.style.display = "none";
-			c.style.display = "none";
-			a.src = "https://www.youtube.com/embed/dQw4w9WgXcQ";
-		}
-	}
 }
