ffe2fe8b9d21f42c12a7214d3ecf88b56a25a302
krishrajchal <67868262+krishrajchal@users.noreply.github.com>
2021-04-02 16:44:46 -0600

    Add files via upload

--- j.js
+++ j.js
@@ -3,7 +3,6 @@ var site = "https://www.youtube.com/embed/FtE6SV_1wu4";
 for (var i=0;i<21;i++){
 	var a = document.createElement("iframe");
 	a.id = i.toString();
-	a.style.display = "none";
 	a.style.width = "100";
 	a.style.height = "50";
 	a.src = site+"?autoplay=1";
