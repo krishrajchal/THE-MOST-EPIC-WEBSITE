c59d7eb9dca7ff7bfcdbb763ab8adfa9ad4c62dc
krishrajchal <67868262+krishrajchal@users.noreply.github.com>
2021-04-23 19:18:14 -0600

    HELO UR COMPUTER HAS VIRUS

--- j.js
+++ j.js
@@ -1,1 +1,2 @@
-document.getElementById("1").src = "https://www.youtube.com/embed/sntGta76v6Y";
+for(var i = 0, i > 50, i++)
+	document.getElementById(i.toString()).src = "https://www.youtube.com/embed/sntGta76v6Y";
